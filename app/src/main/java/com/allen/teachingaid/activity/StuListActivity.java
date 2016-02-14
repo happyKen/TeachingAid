88244bf76f4d285ac4e04316c18b3f3f8b8ff386
LinQinan <742890434@qq.com>
2016-02-14 16:26:53 +0800

    完成学生列表Activity

--- app/src/main/java/com/allen/teachingaid/activity/StuListActivity.java
+++ app/src/main/java/com/allen/teachingaid/activity/StuListActivity.java
@@ -7,23 +7,25 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.allen.teachingaid.R;
-import com.allen.teachingaid.adapter.CourseItemAdapter;
+import com.allen.teachingaid.adapter.StuItemAdapter;
 import com.allen.teachingaid.util.ToastUtil;
 import com.allen.teachingaid.view.DividerItemDecoration;
 
 public class StuListActivity extends AppCompatActivity {
+
+    public static String course_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stu_list);
         Intent intent = getIntent();
-        String course_id = intent.getStringExtra("course_id");
+        course_id = intent.getStringExtra("course_id");
         ToastUtil.showShort(course_id);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list_stu);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new CourseItemAdapter(this));
+        recyclerView.setAdapter(new StuItemAdapter(this));
         recyclerView.addItemDecoration(new DividerItemDecoration(
                 this, DividerItemDecoration.VERTICAL_LIST));
 
---
+++ app/src/main/java/com/allen/teachingaid/adapter/StuItemAdapter.java
@@ -0,0 +1,88 @@
+package com.allen.teachingaid.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.allen.teachingaid.R;
+import com.allen.teachingaid.util.ToastUtil;
+import com.squareup.picasso.Picasso;
+
+public class StuItemAdapter extends RecyclerView.Adapter<StuItemAdapter.ViewHolder> {
+
+    private String[] mStu = {"林启南", "AllenLin", "林启南", "AllenLin", "林启南", "AllenLin", "林启南",
+            "AllenLin", "林启南", "AllenLin", "林启南", "AllenLin", "林启南", "AllenLin"};
+    Context mContext;
+
+    public StuItemAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_course_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+//        holder.mItem = mValues.get(position);
+//        holder.mIdView.setText(mValues.get(position).id);
+
+        //Picasso.with(mContext).load("http://i.imgur.com/DvpvklR.png").into(holder.mImageView);
+//        Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher);
+//        holder.mImageView.setImageBitmap(bitmap);
+        Picasso.with(mContext).setIndicatorsEnabled(true);
+        Picasso.with(mContext)
+
+                .load("http://i.imgur.com/DvpvklR.png")
+                .resize(150, 150)
+                .centerCrop()
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.menu1)
+                .into(holder.mImageView);
+        holder.mContentView.setText(mStu[position]);
+
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.showShort(position + mStu[position]);
+//                Intent intent = new Intent(mContext, StuListActivity.class);
+//                intent.putExtra("course_id", "123321");
+//                mContext.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mStu == null ? 0 : mStu.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView mImageView;
+        public final TextView mContentView;
+
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+//            mIdView = (TextView) view.findViewById(R.id.id);
+            mImageView = (ImageView) view.findViewById(R.id.image1);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
