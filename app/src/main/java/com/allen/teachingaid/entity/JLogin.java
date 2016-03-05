073afc45cea48f461670977cd47fa51998df286b
happyKen <2815506514@qq.com>
2016-03-05 20:58:15 +0800

    添加login模块，json连通成功

---
+++ app/src/main/java/com/allen/teachingaid/entity/JLogin.java
@@ -0,0 +1,18 @@
+package com.allen.teachingaid.entity;
+
+import com.allen.teachingaid.R;
+
+/**
+ * User: zekeng(2815506514@qq.com)
+ * Date: 2016-03-05
+ * Time: 17:04
+ */
+public class JLogin {
+    private String data;//1为成功
+    public void setData(String data){
+        this.data = data;
+    }
+    public String getData(){
+        return data;
+    }
+}
